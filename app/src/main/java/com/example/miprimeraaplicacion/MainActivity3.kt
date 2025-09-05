e680ed832f41ec1abf9d99b903a4e92c65f54650
Johane0405D <Johane0405D@gmail.com>
2025-09-05 11:40:30 -0400

    se añadió una funcion para dividir con try y catch

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity3.kt
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity3.kt
@@ -1,10 +1,15 @@
 package com.example.miprimeraaplicacion
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.miprimeraaplicacion.funciones.OpMatematicas
+
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +17,19 @@ class MainActivity3 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
 
+
+val edvalorUno:EditText =findViewById(R.id.edvalorUno)
+val edValorDos:EditText=findViewById(R.id.edValorDos)
+        val  txResultado:TextView=findViewById(R.id.txResultado)
+        val btn_calcular:Button=findViewById(R.id.btn_calcular)
+
+        btn_calcular.setOnClickListener{
+            //txResultado.text="cambiando texto"
+            var numeroUno:Int=edvalorUno.text.toString().toIntOrNull()?:0
+            var numeroDos:Int=edValorDos.text.toString().toIntOrNull()?:0
+
+            txResultado.text= OpMatematicas.dividirValores(numeroUno,numeroDos)
+        }
 
 
 
---
+++ app/src/main/java/com/example/miprimeraaplicacion/funciones/OpMatematicas.kt
@@ -0,0 +1,16 @@
+package com.example.miprimeraaplicacion.funciones
+
+object OpMatematicas {
+
+    fun dividirValores(
+        valor1:Int, valor2:Int):String{
+        try {
+        var calculo_interno:Int=valor1/valor2
+        return calculo_interno.toString()
+        }catch (e:ArithmeticException){
+            return "404"
+        }finally {
+            println("SE HA CONTROLADO EL ERROR DIVISION BY ZERO")
+        }
+    }
+}
